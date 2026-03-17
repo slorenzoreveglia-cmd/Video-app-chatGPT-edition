83149c1165951aa681715f922d061d2721b75e5e
slorenzoreveglia-cmd <s.lorenzo.reveglia@polofermigiorgi.it>
2026-03-17 11:08:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,19 @@
+const CACHE = "su-streaming";
+
 self.addEventListener("install", e=>{
-    self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"/",
+"index.html",
+"manifest.json"
+]);
+})
+);
 });
 
 self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
 });
